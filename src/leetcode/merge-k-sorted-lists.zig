03a2672641138009c7fcdb71041e204481bb8c82
Ahmed Jaouadi <ahmed.jaouadi@burda-forward.de>
2024-12-14 02:49:52 +0100

    WIP - Adding Runner for leetcode issue

--- src/leetcode/merge-k-sorted-lists.zig
+++ src/leetcode/merge-k-sorted-lists.zig
@@ -1,11 +1,14 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const AllocationError = error{AllocationError};
+const allocator = std.heap.page_allocator;
 
 pub const LinkedList = struct {
     val: i32,
     next: ?*LinkedList,
 };
 
-pub const KSortedLists = struct {
+pub const Solver = struct {
     result: ?*LinkedList,
     const Self = @This();
 
@@ -75,6 +78,59 @@ pub const KSortedLists = struct {
             }
 
             currentResultItem = next;
+        }
+    }
+};
+
+// k == lists.length
+// 0 <= k <= 10^4
+// 0 <= lists[i].length <= 500 --------> lists[i] is the linked list itself and should not have more than 500 items
+// -10^4 <= lists[i][j] <= 10^4 ------> lists[i][j] is LinkedList.val
+// lists[i] is sorted in ascending order.
+// The sum of lists[i].length will not exceed 104.
+pub const Runner = struct {
+    testData: ?*ArrayList(*LinkedList),
+    const Self = @This();
+
+    pub fn init(self: *Self) !*Self {
+        var lists = ArrayList(*LinkedList).init(allocator);
+        self.testData = &lists;
+        const rand = std.crypto.random;
+        const k = rand.intRangeAtMost(u32, 0, 10000);
+
+        for (0..k) |_| {
+            var minVal: i32 = -10000;
+            var lastNode: ?*LinkedList = null;
+            const linkedListLength = rand.intRangeAtMost(u32, 0, 500);
+            for (0..linkedListLength) |linkedListIndex| {
+                var node = LinkedList{ .val = 0, .next = null };
+                if (lastNode) |last| {
+                    minVal = last.*.val;
+                    last.*.next = &node;
+                } else if (self.testData) |data| if (linkedListIndex == 0) {
+                    try data.*.append(&node);
+                };
+
+                node.val = rand.intRangeAtMost(i32, minVal, 10000);
+                lastNode = &node;
+            }
+        }
+
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        if (self.testData) |data| {
+            data.*.deinit();
+            self.testData = null;
+        }
+    }
+
+    pub fn run(self: *Self) void {
+        if (self.testData) |data| {
+            var solverInstance = Solver{ .result = null };
+            const result = solverInstance.init(data.*.items);
+            result.printResult();
         }
     }
 };
